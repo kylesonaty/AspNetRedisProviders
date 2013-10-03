66ffbf0c3b4d7280b62cf02141ffa4b78a082497
Bill Pratt <wpratt83@gmail.com>
2013-10-03 16:57:46 -0400

    Adding RedisProfileProvider class shell

---
+++ RedisProviders/RedisProfileProvider.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Web.Profile;
+
+namespace RedisProviders
+{
+    class RedisProfileProvider : ProfileProvider
+    {
+        private string _applicationName;
+
+        #region "ProviderBase Members"
+
+        public override void Initialize(string name, System.Collections.Specialized.NameValueCollection config)
+        {
+            if (config == null)
+                throw new ArgumentNullException("config");
+
+            base.Initialize(name, config);
+        }
+
+        #endregion
+
+        #region "SettingsProvider Members"
+
+        public override string ApplicationName
+        {
+            get { return _applicationName; }
+            set { _applicationName = value; }
+        }
+
+        public override SettingsPropertyValueCollection GetPropertyValues(SettingsContext context, SettingsPropertyCollection collection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetPropertyValues(SettingsContext context, SettingsPropertyValueCollection collection)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region "ProfileProvider Members"
+
+        public override int DeleteProfiles(ProfileInfoCollection profiles)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int DeleteProfiles(string[] usernames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int DeleteInactiveProfiles(ProfileAuthenticationOption authenticationOption, DateTime userInactiveSinceDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ProfileInfoCollection GetAllProfiles(ProfileAuthenticationOption authenticationOption, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ProfileInfoCollection GetAllInactiveProfiles(ProfileAuthenticationOption authenticationOption, DateTime userInactiveSinceDate, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ProfileInfoCollection FindProfilesByUserName(ProfileAuthenticationOption authenticationOption, string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ProfileInfoCollection FindInactiveProfilesByUserName(ProfileAuthenticationOption authenticationOption, string usernameToMatch, DateTime userInactiveSinceDate, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetNumberOfInactiveProfiles(ProfileAuthenticationOption authenticationOption, DateTime userInactiveSinceDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+    }
+}
