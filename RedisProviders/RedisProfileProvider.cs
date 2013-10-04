f8ee6d36daba31d03b74578b96b8c23167ef89d0
Bill Pratt <wpratt83@gmail.com>
2013-10-03 20:17:08 -0400

    Implementing Initialize function

--- RedisProviders/RedisProfileProvider.cs
+++ RedisProviders/RedisProfileProvider.cs
@@ -9,21 +9,48 @@ namespace RedisProviders
 {
     class RedisProfileProvider : ProfileProvider
     {
-        private string _applicationName;
+        private const string REDIS_CONNECTION_FAILED = "Redis connection failed.";
+        private const string REDIS_PROFILE_PROVIDER = "Redis Profile Provider";
+        private const string EXCEPTION_MESSAGE = "An exception occurred. Please check the Event Log.";
+        private const string PROVIDER_DEFAULT_NAME = "RedisProfileProvider";
 
-        #region "ProviderBase Members"
+        private string _applicationName;
+        private int _redisDb;
+        private string _host;
+        private int _port;
+        private string _password;
+        private bool _writeExceptionsToEventLog;
+
+        #region "System.Configuration.Provider.ProviderBase Members"
 
         public override void Initialize(string name, System.Collections.Specialized.NameValueCollection config)
         {
             if (config == null)
                 throw new ArgumentNullException("config");
 
+            if (string.IsNullOrEmpty(name))
+                name = PROVIDER_DEFAULT_NAME;
+
+            config.Remove("description");
+            config.Add("description", REDIS_PROFILE_PROVIDER);
+
+            // Initialize the abstract base class
             base.Initialize(name, config);
+
+            _host = string.IsNullOrEmpty(config["host"]) ? Constants.RedisDefaultHost : config["host"];
+            _port = string.IsNullOrEmpty(config["port"]) ? Constants.RedisDefaultPort : int.Parse(config["port"]);
+            _password = string.IsNullOrEmpty(config["password"]) ? null : config["password"];
+            _redisDb = string.IsNullOrEmpty(config["db"]) ? Constants.RedisDefaultDB : int.Parse(config["db"]);
+
+            if (config["writeExceptionsToEventLog"] != null && config["writeExceptionsToEventLog"].ToUpper() == "TRUE")
+            {
+                _writeExceptionsToEventLog = true;
+            }
         }
 
         #endregion
 
-        #region "SettingsProvider Members"
+        #region "System.Configuration.SettingsProvider Members"
 
         public override string ApplicationName
         {
@@ -43,7 +70,7 @@ namespace RedisProviders
 
         #endregion
 
-        #region "ProfileProvider Members"
+        #region "System.Web.Profile.ProfileProvider Members"
 
         public override int DeleteProfiles(ProfileInfoCollection profiles)
         {
