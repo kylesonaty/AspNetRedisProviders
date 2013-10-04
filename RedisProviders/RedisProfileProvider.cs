3e2d5eace15a86a5b697d6571a45a818d035b4bc
kylesonaty <kyle.sonaty@gmail.com>
2013-10-03 21:32:55 -0700

    Adding connection logic for Profile provider

--- RedisProviders/RedisProfileProvider.cs
+++ RedisProviders/RedisProfileProvider.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Web.Profile;
+using BookSleeve;
 
 namespace RedisProviders
 {
-    class RedisProfileProvider : ProfileProvider
+    public class RedisProfileProvider : ProfileProvider
     {
         private const string REDIS_CONNECTION_FAILED = "Redis connection failed.";
         private const string REDIS_PROFILE_PROVIDER = "Redis Profile Provider";
         private const string EXCEPTION_MESSAGE = "An exception occurred. Please check the Event Log.";
         private const string PROVIDER_DEFAULT_NAME = "RedisProfileProvider";
+
+        private static RedisConnection _connection;
 
         private string _applicationName;
         private int _redisDb;
@@ -20,8 +25,6 @@ namespace RedisProviders
         private int _port;
         private string _password;
         private bool _writeExceptionsToEventLog;
-
-        #region "System.Configuration.Provider.ProviderBase Members"
 
         public override void Initialize(string name, System.Collections.Specialized.NameValueCollection config)
         {
@@ -48,10 +51,6 @@ namespace RedisProviders
             }
         }
 
-        #endregion
-
-        #region "System.Configuration.SettingsProvider Members"
-
         public override string ApplicationName
         {
             get { return _applicationName; }
@@ -67,10 +66,6 @@ namespace RedisProviders
         {
             throw new NotImplementedException();
         }
-
-        #endregion
-
-        #region "System.Web.Profile.ProfileProvider Members"
 
         public override int DeleteProfiles(ProfileInfoCollection profiles)
         {
@@ -112,7 +107,59 @@ namespace RedisProviders
             throw new NotImplementedException();
         }
 
-        #endregion
 
+        private static void WriteToEventLog(Exception ex, string action)
+        {
+            var log = new EventLog { Source = "RedisMembershipProvider", Log = "Application" };
+            var message = "Action: " + action + "\n\n";
+            message += "Exception: " + ex;
+            log.WriteEntry(message);
+        }
+
+        private RedisConnection GetConnection()
+        {
+            if (_connection == null)
+            {
+                _connection = new RedisConnection(_host, _port, password: _password);
+                _connection.Error += OnConnectionError;
+            }
+
+            if (_connection.State == RedisConnectionBase.ConnectionState.Opening)
+                return _connection;
+
+            if (_connection.State == RedisConnectionBase.ConnectionState.Closing || _connection.State == RedisConnectionBase.ConnectionState.Closed)
+            {
+                try
+                {
+                    _connection = new RedisConnection(_host, _port, password: _password);
+                    _connection.Error += OnConnectionError;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(REDIS_CONNECTION_FAILED, ex);
+                }
+            }
+
+            if (_connection.State == RedisConnectionBase.ConnectionState.Shiny)
+            {
+                try
+                {
+                    var openTask = _connection.Open();
+                    _connection.Wait(openTask);
+                }
+                catch (SocketException ex)
+                {
+                    throw new Exception(REDIS_CONNECTION_FAILED, ex);
+                }
+            }
+
+            return _connection;
+        }
+
+        private void OnConnectionError(object sender, ErrorEventArgs args)
+        {
+            if (_writeExceptionsToEventLog)
+                WriteToEventLog(args.Exception, string.Format("Sender: {0}\n\nCause: {1}", sender, args.Cause));
+        }
     }
 }
