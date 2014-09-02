597a56ab2b78b01a21e06388d975f94fedaf9c8f
Kyle Sonaty <kyle.sonaty@gmail.com>
2014-09-01 21:43:44 -0400

    Added OutputCacheProvider

---
+++ RedisProviders.TestSite/Controllers/OutputCacheController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace RedisProviders.TestSite.Controllers
+{
+    public class OutputCacheController : Controller
+    {
+        //
+        // GET: /OutputCache/
+        [OutputCache(Duration=120,VaryByParam="none")]
+        public ActionResult Index()
+        {
+            ViewBag.Now = DateTime.UtcNow;
+            return View();
+        }
+
+    }
+}
